d1fed1dade6392bfe167de15ddba60fc77a26d98
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-08 12:29:09 +0900

    feat: add Loadig Content

---
+++ src/Shared/Content.js
@@ -0,0 +1,16 @@
+import { AppContext } from '../App/AppProvider';
+
+const Content = (props) => {
+  return (
+    <AppContext.Consumer>
+      {({ coinList }) => {
+        if (!coinList) {
+          return <div>Loading Coins...</div>;
+        }
+        return <div>{props.children}</div>;
+      }}
+    </AppContext.Consumer>
+  );
+};
+
+export default Content;
