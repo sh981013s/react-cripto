706c5a8600678ad586d97a5c3cca765ea78a4241
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-08 12:22:45 +0900

    feat: add Page

---
+++ src/Shared/Page.js
@@ -0,0 +1,16 @@
+import { AppContext } from '../App/AppProvider';
+
+const Page = ({ name, children }) => {
+  return (
+    <AppContext.Consumer>
+      {({ page }) => {
+        if (page !== name) {
+          return null;
+        }
+        return <div>{children}</div>;
+      }}
+    </AppContext.Consumer>
+  );
+};
+
+export default Page;
