f02f03a22b1723dd23a4b25a1ec62fca7bc9ce17
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-08 12:40:50 +0900

    feat: add coinGrid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const CoinGrid = () => {
+  return (
+    <AppContext.Consumer>
+      {({ coinList }) => (
+        <CoinGridStyled>
+          {Object.keys(coinList).map((coinKey) => (
+            <div>{coinKey}</div>
+          ))}
+        </CoinGridStyled>
+      )}
+    </AppContext.Consumer>
+  );
+};
+
+export default CoinGrid;
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,14 @@
 import Welcome from './WelcomeMessage';
 import ConfirmButton from './ConfirmedButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 const Settings = () => {
   return (
     <Page name="settings">
       <Welcome />
       <ConfirmButton />
+      <CoinGrid />
     </Page>
   );
 };
