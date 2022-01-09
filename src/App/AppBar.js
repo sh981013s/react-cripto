c0b6a813a838e3dc132e31f633abdd6b72a5207e
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-09 20:49:03 +0900

    fixed: fixed dashboard firstVisit bug

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -18,13 +18,22 @@ const ControlButtonElem = styled.div`
     css`
       text-shadow: 0px 0px 60px #03ff03;
     `}
+  ${(props) =>
+    props.hidden &&
+    css`
+      display: none;
+    `}
 `;
 
 const ControlButton = ({ name, active }) => {
   return (
     <AppContext.Consumer>
-      {({ page, setPage }) => (
-        <ControlButtonElem active={page === name} onClick={() => setPage(name)}>
+      {({ firstVisit, page, setPage }) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={() => setPage(name)}
+          hidden={firstVisit && name === 'dashboard'}
+        >
           {toProperCase(name)}
         </ControlButtonElem>
       )}
